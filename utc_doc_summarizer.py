c4b0211ccb939d506161ecfe11982c9ca067217c
Shaswat Gupta <shaswatgupta1702@gmail.com>
2025-04-09 18:43:23 +0200

    Add LSA summarizer/

--- utc_doc_summarizer.py
+++ utc_doc_summarizer.py
@@ -2,6 +2,9 @@ import os
 import pandas as pd
 from rake_nltk import Rake
 import yake
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 
 # import nltk
 
@@ -21,8 +24,11 @@ df = df[df["error_message"].isnull()].reset_index(drop=True)
 rake = Rake()
 yake_ext = yake.KeywordExtractor(lan="en", n=3)
 
-rake_summary_proportion = 0.05
-yake_summary_proportion = 0.05
+
+rake_summary_proportion = 0.01
+yake_summary_proportion = 0.01
+lsa_summary_num_sentences = 5
+
 for index, row in df.iterrows():
     doc_num = row["doc_num"].replace('/', '_')
     file_name = f"{doc_num}.txt"
@@ -49,5 +55,15 @@ for index, row in df.iterrows():
             print(f"Keywords for {file_name} : {yake_keywords}")
         except Exception as e:
             print(f"Error extracting yake keywords from {file_name}: {e}")
+            
+        try:
+            parser = PlaintextParser.from_string(text, Tokenizer("english"))
+            summarizer = LsaSummarizer()
+            summary = summarizer(parser.document, sentences_count=lsa_summary_num_sentences)
+            summary_text = " ".join(str(sentence) for sentence in summary)
+            print(f"Summary for {file_name} : {summary_text}")
+        except Exception as e:
+            print(f"Error extracting sumy summary from {file_name}: {e}")
+            
     except Exception as e:
         print(f"Could not open file : {file_name} because of Error : {e}.")
