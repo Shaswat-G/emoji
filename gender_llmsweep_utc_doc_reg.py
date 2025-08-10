5c1a1d35d3cb39278b56d717d7d8bdbf049d234d
Shaswat Gupta <shaswatgupta1702@gmail.com>
2025-08-10 12:05:59 +0200

    Engineer file

--- gender_llmsweep_utc_doc_reg.py
+++ gender_llmsweep_utc_doc_reg.py
@@ -10,19 +10,32 @@ import logging
 from openai import OpenAI
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import multiprocessing
-from utc_finding_proposals_llm_sweep import (load_file, load_config, load_api_key, call_llm_api)
+from utc_finding_proposals_llm_sweep import (
+    load_file,
+    load_config,
+    load_api_key,
+    call_llm_api,
+)
 
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 if __name__ == "__main__":
-    
-    config_file_name = "config.yaml"
-    config_file_path = os.path.join(BASE_PATH, config_file_name)
-    config = load_config(config_file_path)
 
-    api_key_file_path = os.path.join(BASE_PATH, config.get("api_key_file_name", "api_key.txt"))
-    api_key = load_api_key(api_key_file_path)
+    config_file_name = "config_for_finding_gender_docs.yml"
+    config = load_config(os.path.join(BASE_PATH, config_file_name))
     
-    # client = OpenAI(api_key=api_key)
+    api_key = load_api_key(os.path.join(BASE_PATH, config.get("api_key_file_name", "api_key.txt")))
+    user_prompt_template = load_file(os.path.join(BASE_PATH, config.get("user_prompt_file_name")))
+    system_prompt = load_file(os.path.join(BASE_PATH, config.get("system_prompt_file_name")))
+
+    client = OpenAI(api_key=api_key)
+    
+    file_name = "utc_register_with_llm_extraction.xlsx"
+    file_path = os.path.join(BASE_PATH, file_name)
+    df = pd.read_excel(file_path)
+    
+    print(client)
+    print(df.head())
+    
